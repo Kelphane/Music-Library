af656e13ec9b2f3c828260e02bf095fc8e9ce0c6
Dom B <domlamont@yahoo.com>
2021-11-23 12:45:52 -0800

    Created a form in SearchBar that has Select and Search Input Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import NavBar from "./Components/NavBar/NavBar";
+import Axios from "axios";
 
 class App extends Component{
     constructor(props){
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -4,23 +4,39 @@ import { Component } from "react";
 class SearchBar extends Component{
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            filter: " ",
+            searchFor: " "
+        };
     }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        console.log(this.state);
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        
+    }
+    
     render(){
         return (
-            <div>
+            <form onSubmit={this.handleSubmit}>
                 <label htmlFor="filter">Filter: </label>
-
-                <select name="filter" id="filter">
-                    <option value="">--Please choose an option--</option>
+                <select name="filter" id="filter" onChange={this.handleChange}>
+                    <option value="">All</option>
                     <option value="title">Title</option>
                     <option value="album">Album</option>
                     <option value="artist">Artist</option>
                     <option value="genre">Genre</option>
                     <option value="releasedDate">Released Date</option>
                 </select>
-            </div>
+
+                <input type="search" name="searchFor" onChange={this.handleChange}></input>
+            </form>
         );
     }
 }
